67c1b4fc8ad8db2b52c7d4704f21580d758cf0f4
Kaja <kaja.osojnik@gmail.com>
2023-11-28 16:32:56 +0100

    Add box styling and bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import TarotCalculator from './components/TarotCalculator';
 import './App.css'
-import Typography from '@mui/material/Typography';
+import {Typography, Box} from '@mui/material';
+import MainBg from '../src/assets/Artboard11.png'
 
 function App() {
+  const backgroundStyle = {
+    backgroundImage: `url(${MainBg})`, // Specify the URL of the image
+    backgroundSize: 'cover', // Set the background size property
+    backgroundRepeat: 'no-repeat', // Set the background repeat property
+    minHeight: '100vh', // Set the height of the box to fill the viewport
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '80px 0'
+    // Add other styles if needed
+  };
+
   return (
-    <div>
+    <Box 
+      sx={backgroundStyle}
+    >
       <Typography align='center' variant='h1' sx={{fontSize: '40px',
     marginBottom: '24px'}}>Archetypal Portrait Calculator</Typography>
       <TarotCalculator />
-    </div>
+    </Box>
   );
 }
 
